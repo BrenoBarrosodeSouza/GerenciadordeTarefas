9258dcc02aa29f77658a61b9e3d04eaa42d81aa8
Breno <brenobdesouza1@outlook.com>
2023-07-25 00:21:10 -0300

    Adicionando sistema que adiciona, atualiza, deleta e visualiza tarefas

---
+++ app.py
@@ -0,0 +1,32 @@
+import database
+
+while True:
+    print("\n1-Criar tarefa\n2-Visualizar tarefas\n3-Excluir tarefa\n4-Atualizar tarefa\n5-Sair\n")
+    opcao = input()
+    if opcao == "1":
+        print("Qual o título da tarefa?")
+        titulo = input()
+        print("Qual a descrição da tarefa?")
+        descricao = input()
+        print("Qual o status da tarefa?")
+        status = input()
+        database.add_one(titulo, descricao, status)
+
+    if opcao == "2":
+        database.showAll()
+
+    if opcao == "3":
+        database.showAll()
+        print("\nSelecione o id da tarefa que deseja excluir:")
+        tarefaId = input()
+        database.delete_one(tarefaId)
+
+    if opcao == "4":
+        print("\nSelecione o id da tarefa que deseja atualizar:")
+        idStatus = input()
+        print("Qual o novo status da tarefa?")
+        novoStatus = input()
+        database.status_update(idStatus, novoStatus)
+
+    if opcao == "5":
+        break
---
+++ database.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+def showAll():
+ conn=sqlite3.connect('tarefa.db')
+ c=conn.cursor()
+
+ c.execute("SELECT rowid,* FROM tarefas")
+ items=c.fetchall()
+
+ for item in items:
+ 	print(item)
+
+ conn.commit()
+ conn.close()
+
+def add_one(titulo,descricao,status):
+ conn=sqlite3.connect('tarefa.db')
+ c=conn.cursor()
+ c.execute("INSERT INTO tarefas VALUES (?,?,?)",(titulo,descricao,status))
+ conn.commit()
+ conn.close()
+
+def delete_one(id):
+ conn=sqlite3.connect('tarefa.db')
+ c=conn.cursor()
+ c.execute("DELETE FROM tarefas WHERE rowid = (?)",id)
+ conn.commit()
+ conn.close()
+
+def status_update(id_tarefa, novo_status):
+ conn = sqlite3.connect('tarefa.db')
+ c = conn.cursor()
+
+    # Verifica se a tarefa com o ID fornecido existe no banco de dados
+ c.execute("SELECT rowid,* FROM tarefas WHERE rowid = ?", (id_tarefa,))
+ tarefa = c.fetchone()
+
+ if tarefa is None:
+        print("Tarefa não encontrada. Verifique o ID informado.")
+        
+ else:
+
+     c.execute("UPDATE tarefas SET status = ? WHERE rowid = ?", (novo_status, id_tarefa))
+     print("Status da tarefa atualizado com sucesso.")
+     conn.commit()
+     conn.close()
+ 
